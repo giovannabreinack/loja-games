dab7e06b37f53d200ec306cd0b341c52e2473e5b
Gih <giovannabreinackcolombara@gmail.com>
2025-07-01 09:18:16 -0300

    configurações iniciais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234567',
+      database: 'db_lojagames',
+      // entities: [Postagem, Tema],
+      synchronize: true,
+      logging: true,
+    }),
+    // PostagemModule,
+    // TemaModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
